372b65daead9198821eddddff74c0493e04023d8
Arunava-27062003 <arunava.kundu@iemlabs.com>
2024-12-30 17:17:27 +0530

    triangle created

---
+++ src/PatternTwo.java
@@ -0,0 +1,12 @@
+public class PatternTwo {
+    public static void main(String[] args){
+        int n = 4;
+        int sum = 0;
+
+        for(int i = 0; i <= n; i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
